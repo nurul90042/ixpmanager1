2c53f139a3d6f5f01cc1582dcdbb5de0d6498531
nurul90042 <wahdanurul.nw@gmail.com>
2022-08-16 13:15:21 +0700

    improved-test

--- resources/skins/inex/dashboard/dashboard-info.foil.php
+++ resources/skins/inex/dashboard/dashboard-info.foil.php
@@ -4,7 +4,7 @@
         <div class="text-center"><i class="fa fa-info-circle fa-2x "></i></div>
         <div class="col-sm-12">
             A document store, to facilitate transparency and sharing of documents, with the members, by the INEX Board of directors, can be accessed via the
-            <i>Member Information</i> menu item above or by <a href="<?= route('docstore-dir@list', ['dir' => 1 ] ) ?>">clicking here</a>. This document store will be
+            <i>Informasi Member</i> menu item above or by <a href="<?= route('docstore-dir@list', ['dir' => 1 ] ) ?>">clicking here</a>. This document store will be
             populated with future Board, AGM and EGM meeting minutes, on an ongoing basis.
         </div>
     </div>
